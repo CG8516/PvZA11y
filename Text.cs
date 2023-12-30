63603b4d8b58bb4264629e981a952aec3155ef18
Clark <149214880+CG8516@users.noreply.github.com>
2023-12-30 17:04:22 +1100

    Remove accessibility menu constants from code, and load them when language is changed.

--- Text.cs
+++ Text.cs
@@ -115,131 +115,121 @@ namespace PvZA11y
 
             public class Name
             {
-                public string WrapCursorInMenus = "Menu cursor wrapping";
-                public string WrapCursorOnGrids = "Grid cursor wrapping";
-                public string WrapPlantSelection = "Plant selection wrapping";
-                public string KeyRepetition = "Key repetition";
-                public string DoubleTapDelay = "Double tap delay";
-                public string ControllerVibration = "Controller vibration";
-                public string RebindInputs = "Rebind inputs";
-                public string RequireShovelConfirmation = "Shovel Confirmation";
-                public string AutoCollectItems = "Automatic sun collection";
-                public string GameplayTutorial = "Gameplay Tutorials";
-                public string ZombieSonarInterval = "Automatic sonar interval";
-                public string ZombieSonarOnRowChange = "Zombie Sonar on row change";
-                public string ZombieTripwireRow = "Zombie tripwire column";
-                public string ZombieCycleMode = "Zombie cycle mode";
-                public string MoveOnZombieCycle = "Move when cycling zombies.";
-                public string BeghouledMatchAssist = "Be-ghouled match assistance";
-                public string Beghouled2MatchAssist = "Be-ghouled Twist match assistance";
-                public string AutoWakeStinky = "Automatic Snail Bullying";
-                public string SamplePlantOnSwitch = "Click plant when cycled";
-                public string SayTilePosOnMove = "Say board position";
-                public string SayPlantOnTileMove = "Say Plant/Object on cursor movement";
-                public string SayZombieOnTileMove = "Say zombies on cursor tile";
-                public string SayWhenTripwireCrossed = "Say when tripwire has been crossed";
-                public string SaySunCountOnCollect = "Say sun count when collected";
-                public string SayCoinValueOnCollect = "Say coin value when collected";
-                public string SayAvailableInputs = "Say available inputs";
-                public string ScreenReaderEngine = "Screen Reader Engine.";
-                public string MenuPositionCueVolume = "Menu position volume";
-                public string GridPositionCueVolume = "Grid position volume";
-                public string HitBoundaryVolume = "Boundary hit volume";
-                public string PlantSlotChangeVolume = "Selected slot volume";
-                public string AutomaticZombieSonarVolume = "Automatic sonar volume";
-                public string ManualZombieSonarVolume = "Manual sonar volume";
-                public string PlantReadyCueVolume = "Plant ready volume";
-                public string FoundObjectCueVolume = "Plant/Object finder volume";
-                public string FastZombieCueVolume = "Fast zombie alert volume";
-                public string DeadZombieCueVolume = "Zombie death indicator volume";
-                public string ZombieOnTileVolume = "Zombie on tile alert volume";
-                public string ZombieEntryVolume = "Zombie entry alert volume";
-                public string ZombieTripwireVolume = "Zombie tripwire volume";
-                public string BeghouledAssistVolume = "Be-ghouled assistance volume";
-                public string MiscAlertCueVolume = "Miscellaneous alert volume";
-                public string AudioCueMasterVolume = "Master Audio Cue Volume";
-                public string RestartOnCrash = "Restart on crash";
-                public string MoveMouseCursor = "Move mouse cursor";
-                public string AutoLaunchGame = "Automatically launch game";
-                public string Language = "Language";
+                public string WrapCursorInMenus;
+                public string WrapCursorOnGrids;
+                public string WrapPlantSelection;
+                public string KeyRepetition;
+                public string DoubleTapDelay;
+                public string ControllerVibration;
+                public string RebindInputs;
+                public string RequireShovelConfirmation;
+                public string AutoCollectItems;
+                public string GameplayTutorial;
+                public string ZombieSonarInterval;
+                public string ZombieSonarOnRowChange;
+                public string ZombieTripwireRow;
+                public string ZombieCycleMode;
+                public string MoveOnZombieCycle;
+                public string BeghouledMatchAssist;
+                public string Beghouled2MatchAssist;
+                public string AutoWakeStinky;
+                public string SamplePlantOnSwitch;
+                public string SayTilePosOnMove;
+                public string SayPlantOnTileMove;
+                public string SayZombieOnTileMove;
+                public string SayWhenTripwireCrossed;
+                public string SaySunCountOnCollect;
+                public string SayCoinValueOnCollect;
+                public string SayAvailableInputs;
+                public string ScreenReaderEngine;
+                public string MenuPositionCueVolume;
+                public string GridPositionCueVolume;
+                public string HitBoundaryVolume;
+                public string PlantSlotChangeVolume;
+                public string AutomaticZombieSonarVolume;
+                public string ManualZombieSonarVolume;
+                public string PlantReadyCueVolume;
+                public string FoundObjectCueVolume;
+                public string FastZombieCueVolume;
+                public string DeadZombieCueVolume;
+                public string ZombieOnTileVolume;
+                public string ZombieEntryVolume;
+                public string ZombieTripwireVolume;
+                public string BeghouledAssistVolume;
+                public string MiscAlertCueVolume;
+                public string AudioCueMasterVolume;
+                public string RestartOnCrash;
+                public string MoveMouseCursor;
+                public string AutoLaunchGame;
+                public string Language;
             }
 
             public class Description
             {
-                public string WrapCursorInMenus = "Jump to the opposite end of menus when passing the first or last item";
-                public string WrapCursorOnGrids = "Jump to the opposite side of grids when passing the bounds";
-                public string WrapPlantSelection = "Loop selection when cycling plants and Zen Garden tools";
-                public string KeyRepetition = "Repeat directional inputs when held";
-                public string DoubleTapDelay = "Higher values will allow more time to perform a double-tap. Lower values will require faster tapping, but may offer a more responsive experience";
-                public string ControllerVibration = "Whether controller vibration will be used or not";
-                public string RebindInputs = "Allows you to rebind all controls.";
-                public string RequireShovelConfirmation = "Require the shovel button to be pressed twice, to avoid accidental shoveling";
-                public string AutoCollectItems = "Highly recommended until sun collection has been made accessible. Automatically clicks sun, coins, and end-level rewards.";
-                public string GameplayTutorial = "Highly recommended on first playthroughs. Provides helpful gameplay advice.";
-                public string ZombieSonarInterval = "How frequently to perform whole-board zombie sonar sweeps.";
-                public string ZombieSonarOnRowChange = "Which zombie sonar mode to automatically use when changing rows.";
-                public string ZombieTripwireRow = "When any zombie is on, or to the left of this column, an alarm will play.";
-                public string ZombieCycleMode = "Which mode to use when cycling through zombies on the board.";
-                public string MoveOnZombieCycle = "Move your cursor to the zombie's position when cycling zombies";
-                public string BeghouledMatchAssist = "Match assistance mode for the be-ghouled minigame.";
-                public string Beghouled2MatchAssist = "Match assistance for the be-ghouled Twist minigame";
-                public string AutoWakeStinky = "Automatically wake Stinky the Snail, whenever he falls asleep in zen garden mode.";
-                public string SamplePlantOnSwitch = "When cycling plants, automatically pickup and drop the current plant to trigger an in-game sound effect. Causes lag when rapidly cycling through plants";
-                public string SayTilePosOnMove = "Say the current board position when the cursor is moved";
-                public string SayPlantOnTileMove = "Say which plant, gravestone, crater or vase is at the current position, whenever you move around the board.";
-                public string SayZombieOnTileMove = "Says which zombies are on the current tile, when the cursor moves.";
-                public string SayWhenTripwireCrossed = "If zombie tripwire is enabled, say when a zombie has crossed the tripwire.";
-                public string SaySunCountOnCollect = "Say your current sun amount when any sun is collected.";
-                public string SayCoinValueOnCollect = "Say the value of each coin/diamond when it's collected.";
-                public string SayAvailableInputs = "Reads the available inputs when a new dialogue has opened.";
-                public string ScreenReaderEngine = "Which screen reader engine to use, Use left and right to select, and press confirm to apply";
-                public string MenuPositionCueVolume = "Indicates where the cursor is located on a menu or list";
-                public string GridPositionCueVolume = "Indicates where the cursor is located on a grid";
-                public string HitBoundaryVolume = "Indicates when the cursor passes the bounds of an unwrapped grid or list";
-                public string PlantSlotChangeVolume = "Indicates which slot is currently selected";
-                public string AutomaticZombieSonarVolume = "Used for automatic zombie sonar";
-                public string ManualZombieSonarVolume = "Used when zombie sonar for current row is pressed manually";
-                public string PlantReadyCueVolume = "Plays when the current plant is refreshed, and you have enough sun to place it.";
-                public string FoundObjectCueVolume = "When navigating the board, plays a different tone if a plant, gravestone, crater or vase is on the current tile.";
-                public string FastZombieCueVolume = "Plays when a pole-vaulting or football zombie enters the board.";
-                public string DeadZombieCueVolume = "Plays when a zombie dies.";
-                public string ZombieOnTileVolume = "Plays descending tones to indicate the number of zombies on the current tile, when the cursor moves to it.";
-                public string ZombieEntryVolume = "Plays pitched tones to indicate when and where any zombies have entered the lawn.";
-                public string ZombieTripwireVolume = "Background alarm that plays when any zombie is on the left side of the tripwire.";
-                public string BeghouledAssistVolume = "Plays in Be-ghouled minigames, when a match can be found";
-                public string MiscAlertCueVolume = "Used for various alerts throughout the game.";
-                public string AudioCueMasterVolume = "Adjusts the volume of all non-speech audio cues.";
-                public string RestartOnCrash = "Automatically attempt to restart the mod if it crashes";
-                public string MoveMouseCursor = "Move the mouse cursor to visually indicate where clicks will be performed";
-                public string AutoLaunchGame = "Attempt to load the game process, if it's not already running.";
-                public string Language = "Which language to use";
+                public string WrapCursorInMenus;
+                public string WrapCursorOnGrids;
+                public string WrapPlantSelection;
+                public string KeyRepetition;
+                public string DoubleTapDelay;
+                public string ControllerVibration;
+                public string RebindInputs;
+                public string RequireShovelConfirmation;
+                public string AutoCollectItems;
+                public string GameplayTutorial;
+                public string ZombieSonarInterval;
+                public string ZombieSonarOnRowChange;
+                public string ZombieTripwireRow;
+                public string ZombieCycleMode;
+                public string MoveOnZombieCycle;
+                public string BeghouledMatchAssist;
+                public string Beghouled2MatchAssist;
+                public string AutoWakeStinky;
+                public string SamplePlantOnSwitch;
+                public string SayTilePosOnMove;
+                public string SayPlantOnTileMove;
+                public string SayZombieOnTileMove;
+                public string SayWhenTripwireCrossed;
+                public string SaySunCountOnCollect;
+                public string SayCoinValueOnCollect;
+                public string SayAvailableInputs;
+                public string ScreenReaderEngine;
+                public string MenuPositionCueVolume;
+                public string GridPositionCueVolume;
+                public string HitBoundaryVolume;
+                public string PlantSlotChangeVolume;
+                public string AutomaticZombieSonarVolume;
+                public string ManualZombieSonarVolume;
+                public string PlantReadyCueVolume;
+                public string FoundObjectCueVolume;
+                public string FastZombieCueVolume;
+                public string DeadZombieCueVolume;
+                public string ZombieOnTileVolume;
+                public string ZombieEntryVolume;
+                public string ZombieTripwireVolume;
+                public string BeghouledAssistVolume;
+                public string MiscAlertCueVolume;
+                public string AudioCueMasterVolume;
+                public string RestartOnCrash;
+                public string MoveMouseCursor;
+                public string AutoLaunchGame;
+                public string Language;
             }
 
             public class Value
             {
-                public string On = "On.";
-                public string Off = "Off.";
-
-                public string Sonar1 = "Full Sonar.";
-                public string Sonar2 = "Beeps only.";
-                public string Sonar3 = "Count only.";
-
-                public string Beghouled0 = "Hardest, None.";
-                public string Beghouled1 = "Easiest, When current plant can be dragged to make a match.";
-                public string Beghouled2 = "Medium, When current plant can be part of a match, but might not be the one which needs to be dragged.";
-
-                public string Seconds = " seconds.";
-
-                public string ZombieCycle1 = "By ID . Ensures all zombies are cycled through in the same order.";
-                public string ZombieCycle2 = "By distance. Cycle order will change based on zombie distance.";
-
-                public string ScreenreaderWarning = "Warning. This will disable the screenreader. If you are blind or visually impaired, this setting may be difficult to find again.\r\nTo Re-enable the screenreader, press the deny button five times while at the main menu.\r\nIf you want to disable the screenreader, press the ok button again. Otherwise, press the deny button now.";
-
-                public string AutomaticScreenReader = "Automatic.";
-
-                /*
-                
-                 */
+                public string On;
+                public string Off;
+                public string Sonar1;
+                public string Sonar2;
+                public string Sonar3;
+                public string Beghouled0;
+                public string Beghouled1;
+                public string Beghouled2;
+                public string Seconds;
+                public string ZombieCycle1;
+                public string ZombieCycle2;
+                public string ScreenreaderWarning;
+                public string AutomaticScreenReader;
             }
         }
 
@@ -263,6 +253,7 @@ namespace PvZA11y
                 string[] newAchivementDescriptions = deserializer.Deserialize<string[]>(File.ReadAllText(langDir + "\\" + langName + "\\AchievementDescriptions.yaml"));
                 var newTreeDialoge = deserializer.Deserialize<Dictionary<int, string>>(File.ReadAllText(langDir + "\\" + langName + "\\TreeDialogue.yaml"));
                 Tutorial newTutorials = deserializer.Deserialize<Tutorial>(File.ReadAllText(langDir + "\\" + langName + "\\Tutorials.yaml"));
+                Accessibility newAccessibility = deserializer.Deserialize<Accessibility>(File.ReadAllText(langDir + "\\" + langName + "\\AccessibilityMenu.yaml"));
 
                 plantNames = newPlantNames;
                 plantTooltips = newPlantTooltips;
@@ -272,6 +263,7 @@ namespace PvZA11y
                 achievementDescriptions = newAchivementDescriptions;
                 TreeDialogue = newTreeDialoge;
                 tutorial = newTutorials;
+                accessibility = newAccessibility;
                 zombieNames = newZombieNames;
                 Console.WriteLine("Language '{0}' loaded successfully!", langName);
                 Config.current.LanguageID = K4os.Hash.xxHash.XXH32.DigestOf(Encoding.Unicode.GetBytes(langName));
@@ -358,7 +350,7 @@ namespace PvZA11y
             //File.WriteAllText("Language\\English\\AchievementNames.yaml", serializer.Serialize(achievementNames), Encoding.Unicode);
             //File.WriteAllText("Language\\English\\AchievementDescriptions.yaml", serializer.Serialize(achievementDescriptions), Encoding.Unicode);
             //File.WriteAllText("Language\\English\\TreeDialogue.yaml", serializer.Serialize(TreeDialogue), Encoding.Unicode);
-            File.WriteAllText("Language\\English\\AccessibilityMenu.yaml", serializer.Serialize(accessibility), Encoding.Unicode);
+            //File.WriteAllText("Language\\English\\AccessibilityMenu.yaml", serializer.Serialize(accessibility), Encoding.Unicode);
 
             //var deserializer = new YamlDotNet.Serialization.Deserializer();
             //var treedial = deserializer.Deserialize<Dictionary<int, string>>(File.ReadAllText("Language\\English\\TreeDialogue.yaml"));
